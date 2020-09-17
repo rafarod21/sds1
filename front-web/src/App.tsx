be7a305b3d49bed7a39eb730240f022577b90b3c
Rafael Rodrigues <rafael-rafa2.1@hotmail.com>
2020-09-17 09:51:31 -0300

    Adicionando componente Header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import Header from './components/Header';
+
+import './App.css';
+
 function App() {
   return (
-    <h1>
-      Oi
-    </h1>
-  );
+    <div>
+      <Header />
+    </div>
+  )
 }
 
 export default App;
