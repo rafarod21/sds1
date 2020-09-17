13ca14172d1167a7b507d7caf3e462238da0147e
Rafael Rodrigues <rafael-rafa2.1@hotmail.com>
2020-09-17 10:11:02 -0300

    Criando pagina Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from './components/Header';
+import Home from './pages/Home';
 
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   )
 }
